0d5c760a6733d95f70d9556441d9e767e36144c1
aya24k <aya.hata87107176@gmail.com>
2025-01-27 13:58:07 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,25 @@
+public abstract class TangibleAsset extends Asset implements Thing{
+	
+	
+	private int price;
+	private String color;
+	private double weight;	
+	
+	public TangibleAsset(String name,int price ,String Color{
+    this.name =name;
+    this.price=price;
+    this.color=color;
+  }
+	
+	//メソッド
+	
+	public int getPrice() {return this.price;}
+	public String getColor() {return this.color;}
+	public double getWeight() {return this.weight;}
+	
+	public void setPrice(int p) {this.price=p;}
+	public void setColor(String c) {this.color=c;}
+	public void setWeight(double d) {this.weight=d;}		
+
+
+}
